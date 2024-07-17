e31f8401d925d2b29587b4ba11b801436dbf35f7
nakatyom <k.nakano.wz@hotachi-kenki.com>
2024-07-17 20:08:22 +0900

    シュミレーター上で動作するファイルをコミット。

---
+++ app.c
@@ -0,0 +1,29 @@
+#include "app.h"
+#include "list_api.h"
+#include <stdio.h>
+
+/* メインタスク(起動時にのみ関数コールされる) */
+void main_task(intptr_t unused) {
+
+    
+    printf("Start MainTask!!\n");
+    
+    /* ライントレースタスクの起動 */
+    /* write_log(); s*/
+    commn_settings();    
+
+    sta_cyc(NAKA_TASK_CYC);
+
+    /* タスク終了 */
+    ext_tsk();
+}
+
+void naka_task(intptr_t unused){
+    printf("Start Naka Task!!\n");
+
+    
+    /* 左右モータ駆動パワーの設定 */
+    ev3_motor_set_power(left_motor, 90);
+    ev3_motor_set_power(right_motor, -90);
+
+}
---
+++ list_api.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_APP_H
+#define _LIST_APP_H
+#include <stdio.h>
+#include "ev3api.h"
+
+extern void commn_settings();
+
+/* センサーポートの定義 */
+static const sensor_port_t
+    touch_sensor    = EV3_PORT_1,
+    color_sensor    = EV3_PORT_2,
+    sonar_sensor    = EV3_PORT_3,
+    gyro_sensor     = EV3_PORT_4;
+
+/* モーターポートの定義 */
+static const motor_port_t
+    arm_motor       = EV3_PORT_A,
+    left_motor      = EV3_PORT_C,
+    right_motor     = EV3_PORT_B;
+
+    
+#endif	// _LIST_APP_H
