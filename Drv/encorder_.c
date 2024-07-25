d748fcee06c6dd16d20c4e5aa307f7646e2d3b40
masuda-ie <h.masuda.ie@hitachi-kenki.com>
2024-07-25 13:21:12 +0900

    Add files via upload
    角位置を読み出し、printfする関数を定義。

--- Drv/encorder_.c
+++ Drv/encorder_.c
@@ -1,3 +1,13 @@
 #include "ev3api.h"
 #include "encorder_.h"
 #include <stdio.h>
+
+//モーターの角位置を取得してprintf
+void counts_api(motor_port_t lm, motor_port_t rm){
+    
+    lm_counts = ev3_motor_get_counts(lm);
+    rm_counts = ev3_motor_get_counts(rm);
+
+    printf("Motor %d's counts %d, \n Motor %d's counts %d."lm, lm_counts, rm, rm_counts);
+    
+}
--- Drv/encorder_.h
+++ Drv/encorder_.h
@@ -2,8 +2,8 @@
 #define MOTOR_H
 
 // 関数プロトタイプ宣言
-static int32_t counts_api();    // モーター角位置取得
-static int power_api();         // モーターパワー取得
-static ER_UINT type_api();      // モータータイプ取得
+static void counts_api();    // モーター角位置取得
+static void power_api();         // モーターパワー取得
+static void type_api();      // モータータイプ取得
 
 #endif
